fb76c48340ba238cb11c95f21c99e972c8ea69ab
Omar <omar_alqarni@outlook.com>
2024-02-08 10:16:21 +0300

    crud

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ class todoList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     items = db.relationship('todoItem', backref='list', lazy='dynamic')
-    
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     def __repr__(self):
       return f'<Todo List {self.id} {self.title}>'
 
@@ -74,7 +74,7 @@ def hello_world():
     return 'Hello World!'
 
 
-
+########## user routes ##########
 # signup route
 @app.route('/signup', methods=['POST'])
 def signUp():
@@ -110,17 +110,148 @@ def login():
   access_token = create_access_token(identity=username)
   return jsonify(access_token=access_token), 200
 
+# get user profile
+@app.route('/profile', methods=['GET'])
+@jwt_required()
+def profile():
+  user = get_jwt_identity()
+  return jsonify(username=user), 200
 
-# create a new todo list
+# update user profile
+@app.route('/profile', methods=['PUT'])
+@jwt_required()
+def updateProfile():
+  username = request.json.get("username", None)
+  email = request.json.get("email", None)
+  user = get_jwt_identity()
+  user = User.query.filter_by(username=user).first()
+  user.username = username
+  user.email = email
+  db.session.commit()
+  return jsonify(username=username, email=email), 200
+
+
+
+########## todo list routes ##########
+
+# create todo list
 @app.route('/lists', methods=['POST'])
 @jwt_required()
 def createList():
   title = request.json.get("title", None)
-  user = User.query.filter_by(username=get_jwt_identity()).first()
-  newList = todoList(title=title, user=user)
-  db.session.add(newList)
+  user = get_jwt_identity()
+  list = todoList(title=title, owner_id=User.query.filter_by(username=user).first().id)
+  db.session.add(list)
   db.session.commit()
-  return jsonify(id=newList.id, title=newList.title)
+  return jsonify(title=title),200
+
+# get all todo lists
+@app.route('/lists', methods=['GET'])
+@jwt_required()
+def getAllLists():
+  user = get_jwt_identity()
+  lists = todoList.query.filter_by(owner_id=User.query.filter_by(username=user).first().id).all()
+  return jsonify([list.title for list in lists]), 200
+
+# get a single todo list
+@app.route('/lists/<int:id>', methods=['GET'])
+@jwt_required()
+def getAList(id):
+  list = todoList.query.filter_by(id=id).first()
+  return jsonify(title=list.title, items=[item.title for item in list.items.all()]), 200
+
+# update a todo list title
+@app.route('/lists/<int:id>', methods=['PUT'])
+@jwt_required()
+def updateListTitle(id):
+  title = request.json.get("title", None)
+  list = todoList.query.filter_by(id=id).first()
+  list.title = title
+  db.session.commit()
+  return jsonify(title=title), 200
+
+# add todo item to a list
+@app.route('/lists/<int:id>/items', methods=['POST'])
+@jwt_required()
+def addItem(id):
+  title = request.json.get("title", None)
+  done = request.json.get("done", False)
+  item = todoItem(title=title, done=done, list_id=id)
+  db.session.add(item)
+  db.session.commit()
+  return jsonify(title=title, done=done), 200
+
+# remove todo item from a list
+@app.route('/lists/<int:list_id>/items/<int:item_id>', methods=['DELETE'])
+@jwt_required()
+def removeItem(list_id, item_id):
+  list = todoList.query.filter_by(id=list_id).first()
+  if not list:
+    return jsonify(message="List not found"), 404
+  if not list.items.filter_by(id=item_id).first():
+    return jsonify(message="Item not found"), 404
+  item = todoItem.query.filter_by(id=item_id).first()
+  db.session.delete(item)
+  db.session.commit()
+  return jsonify(message="Item deleted"), 200
+
+# update todo item in a list
+@app.route('/lists/<int:list_id>/items/<int:item_id>', methods=['PUT'])
+@jwt_required()
+def updateItem(list_id, item_id):
+  title = request.json.get("title", None)
+  done = request.json.get("done", None)
+  item = todoItem.query.filter_by(id=item_id).first()
+  item.title = title
+  item.done = done
+  db.session.commit()
+  return jsonify(title=title, done=done), 200
+
+# delete a todo list
+@app.route('/lists/<int:id>', methods=['DELETE'])
+@jwt_required()
+def deleteList(id):
+  list = todoList.query.filter_by(id=id).first()
+  db.session.delete(list)
+  db.session.commit()
+  return jsonify(message="List deleted"), 200
+
+########## admin routes ##########
+@app.route('/admin')
+@jwt_required()
+def admin():
+  user = get_jwt_identity()
+  if user != "admin":
+    return jsonify(message="You are not an admin"), 403
+  return jsonify(message="Welcome admin")
+
+#  create a user
+@app.route('/admin/users', methods=['POST'])
+@jwt_required()
+def createUser():
+  user = get_jwt_identity()
+  if user != "admin":
+    return jsonify(message="You are not an admin"), 403
+  username = request.json.get("username", None)
+  email = request.json.get("email", None)
+  password = request.json.get("password", None)
+  hashedPassword = hashlib.sha256(password).hexdigest()
+  user = User(username=username, email=email, password=hashedPassword)
+  db.session.add(user)
+  db.session.commit()
+  return jsonify(username=username, email=email), 200
+
+# delete a user
+@app.route(('/admin/users/<int:id>/delete'), methods=['DELETE'])
+@jwt_required()
+def delete_user():
+  user = get_jwt_identity()
+  if user != "admin":
+    return jsonify(message="You are not an admin"), 403
+  user = User.query.filter_by(id=id).first()
+  db.session.delete(user)
+  db.session.commit()
+  return jsonify(message="User deleted"), 200
 
 
 
